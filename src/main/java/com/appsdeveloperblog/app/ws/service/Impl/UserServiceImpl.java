dd92016dc68b208625a35d83c9c0e33dacc8ad99
Mark Darroch <mgdarroch@gmail.com>
2021-01-28 14:52:46 +0000

    User ID is now generated. No longer hardcoded.

--- src/main/java/com/appsdeveloperblog/app/ws/service/Impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/Impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.app.ws.service.Impl;
 import com.appsdeveloperblog.app.ws.UserRepository;
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.Utils;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,20 +13,26 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     UserRepository userRepository;
+    Utils utils;
 
     // Constructor Injection which is Autowired automatically.
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, Utils utils) {
         this.userRepository = userRepository;
+        this.utils = utils;
     }
 
     @Override
     public UserDto createUser(UserDto userDto) {
 
+        // A different way from @Column(unique="true") to enforce unique values.
+        if(userRepository.findUserByEmail(userDto.getEmail()) != null) throw new RuntimeException("Record already exists.");
+
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(userDto, userEntity);
 
+        String publicUserId = utils.generateUserId(30);
+        userEntity.setUserId(publicUserId);
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
 
